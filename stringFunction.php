2da279af45d2f9bc5bef3c52cacc88367c7fc298
宸铭 <progpark@outlook.com>
2014-07-15 09:30:53 +0800

    Update stringFunction.php

--- stringFunction.php
+++ stringFunction.php
@@ -21,3 +21,35 @@ function clearHrefForText($sText) {
 
     return $sNewText;
 }
+
+/**
+ * 生成随机字符串
+ *
+ * @param integer $length 要生成的随机字符串长度 
+ * @param string  $type   随机码类型：0，数字+字母；1，数字；2，小写字母；3，大写字母；4，特殊字符；-1，数字+字母+特殊字符
+ *
+ * @return string   
+ */
+function randomString($length = 6, $type = 0) {
+
+    $arr = array(1=>"0123456789", 2=>"abcdefghijklmnopqrstuvwxyz", 3=>"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 4=>"~@#$%^&*(){}[]|");
+
+    if ($type == 0) {
+        array_pop($arr);
+        $string = implode("", $arr);
+    } elseif ($type == "-1") {
+        $string = implode("", $arr);
+    } else {
+        $string = $arr[$type];
+    }
+
+    $count = strlen($string) - 1;
+    $code = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $code .= $string[rand(0, $count)];
+    }
+
+    return $code;
+}
+
